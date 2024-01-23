79483586b199bb232be448baa8aa128c6efa76fe
gozneokhan <gozneokhan@gmail.com>
2024-01-23 17:55:41 +0900

    gitaction-test4

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-    }),
-  );
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+        })
+    );
 
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept , Authorization , X-XSRF-TOKEN',
-    credentials: true,
-  });
+    app.enableCors({
+        origin: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type, Accept , Authorization , X-XSRF-TOKEN',
+        credentials: true,
+    });
 
-  await app.listen(4000, '0.0.0.0');
+    await app.listen(5000, '0.0.0.0');
 }
 
 bootstrap();
